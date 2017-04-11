5d270cb91273518245282fa3662fc8425204b645
Alastair Doughty <alastairdoughty@yahooo.com>
2017-04-11 14:46:53 +0100

    First commit from laptop.

--- colourgame.js
+++ colourgame.js
@@ -8,8 +8,9 @@ var colours = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColour = colours[3];
+var pickedColour = pickColour();
 var colourDisplay = document.getElementById("colourDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colourDisplay.textContent = pickedColour;
 
@@ -23,9 +24,23 @@ for(var i = 0; i < squares.length; i++){
     var clickedColour = this.style.background;
     // compare colour to picked colour
     if(clickedColour === pickedColour){
-      alert("correct!");
+      messageDisplay.textContent = "Correct!";
+      changeColours(clickedColour);
     } else {
-      alert("WRONG!!!");
+      this.style.background = "#232323";
+      messageDisplay.textContent = "Try Again";
     }
   })
+}
+
+function changeColours(colour){
+  // loop through all squares
+  for(var i = 0; i < squares.length; i++){
+  // change each colour to match given colours
+    squares[i].style.background = colour;
+  }
+}
+
+function pickColour(){
+  math.random()
 }
